18dd237f4796006771e57b9507a99d5baff900a5
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-05-02 12:45:30 +0200

    finalize task2

--- client.js
+++ client.js
@@ -1,22 +1,67 @@
+function getSingleVideoRequest(videoInfo) {
+  const videoRequestContainerElement = document.createElement("div"); // this is node
+  // binding data
+  videoRequestContainerElement.innerHTML = `
+  <div class="card mb-3">
+    <div class="card-body d-flex justify-content-between flex-row">
+      <div class="d-flex flex-column">
+        <h3>${videoInfo.topic_title}</h3>
+        <p class="text-muted mb-2">${videoInfo.topic_details}</p>
+        <p class="mb-0 text-muted">
+          <strong>Expected results:</strong> ${videoInfo.expected_result}
+        </p>
+      </div>
+      <div class="d-flex flex-column text-center">
+        <a class="btn btn-link">🔺</a>
+        <h3>0</h3>
+        <a class="btn btn-link">🔻</a>
+      </div>
+      </div>
+      <div class="card-footer d-flex flex-row justify-content-between">
+      <div>
+        <span class="text-info">${videoInfo.status.toUpperCase()}</span>
+        &bullet; added by <strong>${videoInfo.author_name}</strong> on
+        <strong>${new Date(videoInfo.submit_date).toLocaleDateString()}</strong>
+      </div>
+      <div
+        class="d-flex justify-content-center flex-column 408ml-auto mr-2"
+      >
+        <div class="badge badge-success">
+          ${videoInfo.target_level}
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  return videoRequestContainerElement;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   /*
-            this will fire after dom elements loaded and before images and css loaded
-            but
-            window.load event => will fire when every thing is loaded
-          */
-  const formVidReq = document.getElementById("formVideoRequest");
-  let videos = [];
+    this will fire after dom elements loaded and before images and css loaded
+    but
+    window.load event => will fire when every thing is loaded
+  */
+  const formVideoRequestElement = document.getElementById("formVideoRequest");
+  const listOfVideoesElement = document.getElementById("listOfRequests");
 
-  // loadVideos();
-
-  formVidReq.addEventListener("submit", (e) => {
+  fetch("//localhost:7777/video-request")
+    .then((blob) => blob.json())
+    // .then((data) => console.log(data))
+    .then((data) => {
+      data.forEach((videoInfo) => {
+        // debugger;
+        listOfVideoesElement.appendChild(getSingleVideoRequest(videoInfo));
+      });
+    });
+  formVideoRequestElement.addEventListener("submit", (e) => {
     e.preventDefault();
     /*
-            fetch("//localhost:7777/video-request");
-            this will make get request and reply with promise
-            */
+      fetch("//localhost:7777/video-request");
+      this will make get request and reply with promise
+    */
+    const formData = new FormData(formVideoRequestElement);
 
-    const formData = new FormData(formVidReq);
     fetch("//localhost:7777/video-request", {
       method: "POST",
       /*
@@ -29,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
           topic_details: "Eum ut earum enim es",
           expected_result: "Sed sequi veniam ut",
         }),
-        */
+      */
       // another way to send data with form data
       body: formData, // this expect in server to deal with data multi part like you upload file
     })
@@ -39,21 +84,3 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
-
-function loadVideos() {
-  let request = new XMLHttpRequest();
-  request.addEventListener("load", (e) => {
-    videos = JSON.parse(request.response);
-    let referenceCard = document.getElementsByClassName("card")[0];
-    referenceCard.setAttribute("style", "display:none");
-    videos.forEach((element, key) => {
-      let card = referenceCard.cloneNode(true);
-      card.childNodes[1].childNodes[1].childNodes[1].textContent =
-        "Title " + key;
-      card.style = "";
-      document.getElementById("listOfRequests").appendChild(card);
-    });
-  });
-  request.open("get", "//localhost:7777/video-request");
-  request.send();
-}
